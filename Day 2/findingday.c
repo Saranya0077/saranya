5fc456951a98bebcbd84a49fa356dacc595dadd0
Saranya0077 <kitesaranya007@gmail.com>
2025-06-17 10:57:17 +0530

    Create findingday.c

---
+++ Day 2/findingday.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int day;
+    printf("enter a number");
+    scanf("%d",&day);
+    if(day==1){
+        printf("day is sunday");}
+        else if(day==2){
+            printf("day is monday");}
+        else if(day==3){
+            printf("day is tuesday");}
+        else if(day==4){
+            printf("day is wednesday");}
+        else if(day==4){
+            printf("day is thursday");}
+
+    return 0;
+}

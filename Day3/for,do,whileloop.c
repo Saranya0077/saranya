d189405eaf7c4e294d74e74b57d3daa2d59eeec5
Saranya0077 <kitesaranya007@gmail.com>
2025-06-17 13:08:54 +0530

    Create for,do,whileloop.c

---
+++ Day3/for,do,whileloop.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int i;
+int main() {
+    for (int i = 0; i <= 5; i++) {
+        printf("%d", i);
+    }
+    printf("\n");
+    i=0;
+    while(i <= 5){
+        printf("%d",i);
+        i++;}
+        printf("\n");
+        
+        
+        
+    
+    i=0;
+    do{
+        printf("%d",i);
+        i++;}
+    while(i <= 5);
+    return 0;
+        
+    
+   
+}

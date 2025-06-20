8e285d72699cc7aaacbca27adf62b79605ae744a
Saranya0077 <kitesaranya007@gmail.com>
2025-06-20 21:38:21 +0530

    Create primenumber.c

---
+++ day4/primenumber.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  int val=16,i;
+  for(int i=2;i<val-1;i++){
+      if(val%i==0){
+      
+          printf("its not a prime number");}
+          break;
+      
+      
+  } 
